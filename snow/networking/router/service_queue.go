d59cda2189d73d310b0b35a5b875d2092871e16b
Dan Laine <daniel.laine@avalabs.org>
2020-10-20 18:07:04 -0400

    prioritize query messages

--- snow/networking/router/service_queue.go
+++ snow/networking/router/service_queue.go
@@ -11,6 +11,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 
 	"github.com/ava-labs/avalanchego/ids"
+	"github.com/ava-labs/avalanchego/utils/constants"
 	"github.com/ava-labs/avalanchego/utils/logging"
 )
 
@@ -243,6 +244,9 @@ func (ml *multiLevelQueue) placeMessage(msg message) bool {
 	// Find the highest index this message could be placed on and waterfall
 	// the message to lower queues if the higher ones are full
 	queueIndex := ml.getPriorityIndex(msg.validatorID)
+	if msg.messageType == constants.PullQueryMsg || msg.messageType == constants.PushQueryMsg || msg.messageType == constants.GetMsg {
+		queueIndex = 0 // todo remove this if block
+	}
 	return ml.waterfallMessage(msg, queueIndex)
 }
 
